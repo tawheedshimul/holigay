56367fd2e944755b7e3134ed5d1c21cbf5734af5
unknown <you@example.com>
2024-02-07 01:31:14 +0600

    first commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,56 @@
+<template>
+    <nav class="navbar">
+        <div class="navbar-left">
+            <div class="navbar-item">Holigay</div>
+        </div>
+        <div class="navbar-right">
+            <div class="navbar-item">My Account</div>
+            <div class="navbar-item">Guide</div>
+            <button class="toggle-btn" @click="toggleSidebar">Toggle</button>
+        </div>
+    </nav>
+</template>
+  
+<script setup>
+import { ref } from 'vue';
+
+const toggleMenu = ref(false);
+
+function toggleSidebar() {
+    toggleMenu.value = !toggleMenu.value;
+}
+</script>
+  
+<style scoped>
+.navbar {
+    background-color: #333;
+    color: #fff;
+    padding: 10px;
+    display: flex;
+    justify-content: space-between;
+    opacity: 60%;
+    display: fixed;
+}
+
+.navbar-right {
+    display: flex;
+}
+
+.navbar-item {
+    margin: 0 10px;
+}
+
+.toggle-btn {
+    background-color: transparent;
+    color: #fff;
+    border: none;
+    cursor: pointer;
+}
+
+@media (min-width: 1024px) {
+    .navbar {
+        padding: 10px 30px;
+    }
+}
+</style>
+  
