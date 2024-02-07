2f85779b555f738882377c33a8e3715d80526460
unknown <you@example.com>
2024-02-07 22:08:23 +0600

    update

--- src/components/Room.vue
+++ src/components/Room.vue
@@ -1,15 +1,75 @@
 <template>
     <div class="room">
         <div class="room-top">
-            <p>
+            <p class="room-title">
                 2Rooms Available
             </p>
             <div>
-
+hello
             </div>
         </div>
+        <!-- rooom 1  -->
         <div class="room-card">
             <p class="room-card-title">Double Standard Room</p>
+            <div class="room-card-object">
+                <div class="room-card-left">
+                    <img src="../assets/showone.png" alt="">
+                    <p>room</p>
+
+                </div>
+                <div class="vertical-line"></div>
+                <div class="room-card-middle">
+                    <span class="card-text">Wi-fi </span>
+                    <span class="card-text">Shower </span>
+                    <span class="card-text">City View </span>
+                    <span class="card-text">Hair Dryer </span>
+                    <span class="card-text">Towel </span>
+                    <span class="card-text">Free toiletries </span>
+                    <span class="card-text">Kitchen </span>
+                    <span class="card-text">Washing machine </span>
+                    <span class="card-text">Toilet </span>
+                    <span class="card-text">Sofa </span>
+                    <span class="card-text">Linen </span>
+                    <span class="card-text">Desk </span>
+                    <span class="card-text">Seating Area </span>
+                    <span class="card-text">Video </span>
+                    <span class="card-text">Private entrance </span>
+                    <span class="card-text">TV </span>
+                    <span class="card-text">internet Facilites </span>
+                    <span class="card-text">Refrigerator </span>
+                    <span class="card-text">Ironing </span>
+                    <span class="card-text">Tea/Coffee maker </span>
+                    <span class="card-text">MicroWave </span>
+                    <span class="card-text">Heating </span>
+                    <span class="card-text">Kitchenware </span>
+                    <span class="card-text">Fan </span>
+                    <span class="card-text">Outdoor furniture </span>
+                </div>
+                <div class="vertical-line"></div>
+                <div>
+                    <div style="display: flex ; align-items: last baseline;">
+                        <p
+                            style="font-family: 'Plus Jakarta Sans', sans-serif; font-weight: 600; font-size: 32px;  letter-spacing: -2%;margin: 0px; color: #040112;">
+                            $86</p>
+                        <span class="text-low-opacity">/night</span>
+                    </div>
+
+                    <form class="card-pay">
+                        <input type="radio" id="paycard" name="paymentMethod">
+                        <label class="card-text2" for="paycard">Pay with card</label>
+                        <br>
+                        <input type="radio" id="payhotel" name="paymentMethod">
+                        <label class="card-text2" for="payhotel"><span>Pay at hotel</span></label>
+                    </form>
+                    <button class="btn">Book Now</button>
+                    <p class="text-low-opacity">Free Cancellation before 18 Apr 2024</p>
+                </div>
+            </div>
+
+        </div>
+        <!-- room 2  -->
+        <div class="room-card">
+            <p class="room-card-title">Twin Room</p>
             <div class="room-card-object">
                 <div class="room-card-left">
                     <img src="../assets/showone.png" alt="">
@@ -76,11 +136,26 @@
     margin: auto;
 }
 
+.room-top{
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+.room-title {
+    font-family: 'Plus Jakarta Sans', sans-serif;
+    font-weight: 600;
+    font-size: 24px;
+    line-height: 28.8px;
+    letter-spacing: -2%;
+    color: #040112;
+}
+
 .room-card {
     width: 100%;
     background-color: #F7F7F8;
     border-radius: 16px;
     padding: 24px;
+    margin-top: 10px;
 }
 
 .room-card-title {
@@ -123,12 +198,12 @@
     line-height: 23.8px;
     color: #040112;
     border-radius: 4px;
-    padding: 2px 4px; 
-    margin-right: 10px; 
-    background-color: #EDEDEF; 
+    padding: 2px 4px;
+    margin-right: 10px;
+    background-color: #EDEDEF;
 }
 
-.card-text2{
+.card-text2 {
     font-family: 'Plus Jakarta Sans', sans-serif;
     font-weight: 400;
     font-size: 14px;
@@ -172,5 +247,4 @@ input[type="radio"]:checked {
     line-height: 23.8px;
     color: #040112;
     opacity: 60%;
-}
-</style>
+}</style>
